bc45dd09bdcc98af2280ae573d177be4c748ce8a
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-09-02 23:14:13 +0300

    Ex 2.1.

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -1,8 +1,39 @@
 import React from 'react'
 
-const Course = ( {course} ) => {
+
+const Header = ({ name }) => {
   return (
     <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+
+const Part = ({ name, exercises }) =>{
+  return (
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
     </div>
   )
 }
