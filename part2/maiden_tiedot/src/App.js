57d5feafa9463f7f8eb2e935110b48b78a81826f
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-09-04 00:12:31 +0300

    Ex 2.12.

--- part2/maiden_tiedot/src/App.js
+++ part2/maiden_tiedot/src/App.js
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App = () => {
+const Country = ({ country }) => {
   return (
     <div>
+      <h1>{country.name}</h1>
+      <div>
+        Capital: {country.capital}
+        <br></br>
+        Population: {country.population}
+      </div>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map(language =>
+          <li key={language.name}>{language.name}</li>
+        )}
+      </ul>
+      <img src={country.flag} alt="flag" width="320" height="320"/>
+    </div>
+  )
+}
 
+const CountriesDisplay = ({ countries }) => {
+  if (countries.length > 10) {
+    return (
+      <div></div>
+    )
+  }
+  if (countries.length === 1) {
+    return (
+      <Country country={countries[0]} />
+    )
+  }
+  return (
+    <div>
+      {countries.map(country =>
+        <div key={country.name}>{country.name}</div>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then((response) => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const countriesToShow = countries.filter(country =>
+    country.name.toLowerCase().includes(newFilter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <div>
+        <form>
+          Find countries:
+          <input
+            value={newFilter}
+            onChange={handleFilterChange}
+          />
+        </form>
+      </div>
+      <div>
+        <CountriesDisplay countries={countriesToShow} />
+      </div>
     </div>
   )
 }
