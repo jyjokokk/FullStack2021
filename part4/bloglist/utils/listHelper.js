ee919e22aba7b9cf079f64c3157ce784378000f5
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2022-02-11 06:52:51 +0200

    Ex 4.4.

--- part4/bloglist/utils/listHelper.js
+++ part4/bloglist/utils/listHelper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
+  if (!blogs) {
+    console.log(blogs)
+  }
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const summer = (prev, current) => prev + current.likes
+  // let total = 0;
+  // blogs.forEach(item => total += item.likes)
+  return blogs.reduce(summer, 0)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
